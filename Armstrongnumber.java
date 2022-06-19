171ac3e4099057def5791ac3e6b33757acbe7972
mailgsprabhu <59953883+mailgsprabhu@users.noreply.github.com>
2022-06-19 15:14:09 +0530

    initial commit

---
+++ Armstrongnumber.java
@@ -0,0 +1,32 @@
+package tamilnadu.chennai;
+
+public class Armstrongnumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Armstrongnumber a= new Armstrongnumber();
+		
+		a.armstrong(370);
+	}
+
+	private void armstrong(int g) {
+		// TODO Auto-generated method stub
+		int no=g;
+		int arm=no;
+		int rem=0;
+		int f=0;
+		while(no>0) {
+			rem=no%10;
+			f=(rem*rem*rem)+f;
+			no=no/10;
+		}
+		if(f==arm) {
+			System.out.println(arm +" number is armstrong");
+		}
+		else {
+			System.out.println("not armstrong...");
+		}
+	}
+
+}
